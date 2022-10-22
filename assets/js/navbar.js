9d7dc9f30e89e9bf5523a22577e2de3200f06e35
nnunun08b <sms0825@naver.com>
2022-10-22 16:12:27 +0900

    UCL coading

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -10,95 +10,95 @@
   // Variables
   // =========
 
-  // var navbar = document.querySelector('.navbar');
-  // var navbarCollapse = document.querySelector('.navbar-collapse');
+  var navbar = document.querySelector('.navbar');
+  var navbarCollapse = document.querySelector('.navbar-collapse');
 
-  // var isLight = false;
-  // var isCollapsed = false;
-  // var isTogglable = navbar.classList.contains('navbar-togglable');
+  var isLight = false;
+  var isCollapsed = false;
+  var isTogglable = navbar.classList.contains('navbar-togglable');
 
   // Methods
   // =======
 
-  // function makeNavbarLight() {
+  function makeNavbarLight() {
 
-  //   if ( !isLight && isTogglable ) {
-  //     navbar.classList.remove('navbar-dark');
-  //     navbar.classList.add('navbar-light');
+    if ( !isLight && isTogglable ) {
+      navbar.classList.remove('navbar-dark');
+      navbar.classList.add('navbar-light');
 
-  //     isLight = true;
-  //   }
-  // }
+      isLight = true;
+    }
+  }
   
-  // function makeNavbarDark() {
+  function makeNavbarDark() {
 
-  //   if ( isLight && isTogglable ) {
-  //     navbar.classList.remove('navbar-light');
-  //     navbar.classList.add('navbar-dark');
+    if ( isLight && isTogglable ) {
+      navbar.classList.remove('navbar-light');
+      navbar.classList.add('navbar-dark');
 
-  //     isLight = false;
-  //   }
-  // }
+      isLight = false;
+    }
+  }
 
-  // function toggleNavbar(event, index) {
+  function toggleNavbar(event, index) {
 
-  //   if ( event == 'fpOnLeave' ) {
+    if ( event == 'fpOnLeave' ) {
 
-  //     if ( index == 0 ) {
-  //       makeNavbarDark();
-  //     } else {
-  //       makeNavbarLight();
-  //     }
+      if ( index == 0 ) {
+        makeNavbarDark();
+      } else {
+        makeNavbarLight();
+      }
 
-  //   } else if ( event == 'collapse' ) {
-  //     var scrollTop = window.pageYOffset;
+    } else if ( event == 'collapse' ) {
+      var scrollTop = window.pageYOffset;
 
-  //     if ( scrollTop == 0 && isCollapsed ) {
-  //       makeNavbarDark();
-  //     } else {
-  //       makeNavbarLight();
-  //     }
+      if ( scrollTop == 0 && isCollapsed ) {
+        makeNavbarDark();
+      } else {
+        makeNavbarLight();
+      }
 
-  //   } else if ( event == 'scroll' || event == 'load' ) {
-  //     var scrollTop = window.pageYOffset;
+    } else if ( event == 'scroll' || event == 'load' ) {
+      var scrollTop = window.pageYOffset;
 
-  //     if ( scrollTop == 0 && !isCollapsed && isLight ) {
-  //       makeNavbarDark();
-  //     } else if ( scrollTop !== 0 && !isLight ) {
-  //       makeNavbarLight();
-  //     }
+      if ( scrollTop == 0 && !isCollapsed && isLight ) {
+        makeNavbarDark();
+      } else if ( scrollTop !== 0 && !isLight ) {
+        makeNavbarLight();
+      }
 
-  //   }
+    }
 
-  // }
+  }
 
   // Events
   // ======
 
   // Window events
 
-  // "load scroll fpOnLeave".split(' ').forEach(function(e) {
-  //   window.addEventListener(e, function(e) {
-  //     var type = e.type;
-  //     var index = ( e.detail ) ? e.detail.destination.index : undefined;
+  "load scroll fpOnLeave".split(' ').forEach(function(e) {
+    window.addEventListener(e, function(e) {
+      var type = e.type;
+      var index = ( e.detail ) ? e.detail.destination.index : undefined;
 
-  //     toggleNavbar(type, index);
-  //   });
-  // });
+      toggleNavbar(type, index);
+    });
+  });
 
   // Collapse events
 
-  // $(navbarCollapse).on({
-  //   'show.bs.collapse': function() {
-  //     toggleNavbar('collapse');
+  $(navbarCollapse).on({
+    'show.bs.collapse': function() {
+      toggleNavbar('collapse');
 
-  //     isCollapsed = true;
-  //   },
-  //   'hidden.bs.collapse': function() {
-  //     toggleNavbar('collapse');
+      isCollapsed = true;
+    },
+    'hidden.bs.collapse': function() {
+      toggleNavbar('collapse');
 
-  //     isCollapsed = false;
-  //   }
-  // });
+      isCollapsed = false;
+    }
+  });
 
 })();
